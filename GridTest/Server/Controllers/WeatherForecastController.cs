27bddc5c203d1eb5b83bfdaabc885c633f3755a9
Ken Dudley <github@dqcs.com>
2021-05-21 02:49:55 -0400

    adding

---
+++ GridTest/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GridTest.Shared;
+
+namespace GridTest.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        private static List<WeatherForecast> wfl = new List<WeatherForecast>();
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return wfl;
+        }
+
+        [HttpPost]
+        public ActionResult Post(WeatherForecast w)
+        {
+            w.key = Guid.NewGuid();
+            wfl.Add(w);
+            return Ok(w);
+        }
+
+        [HttpPut]
+        public ActionResult Put(WeatherForecast w)
+        {
+            WeatherForecast myr = (from x in wfl where x.key == w.key select x).FirstOrDefault<WeatherForecast>();
+            wfl.Remove(myr);
+            wfl.Add(w);
+
+            return Ok(w);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(Guid key)
+        {
+            WeatherForecast myr = (from x in wfl where x.key == key select x).FirstOrDefault<WeatherForecast>();
+            wfl.Remove(myr);
+
+            return Ok();
+        }
+    }
+}
---
+++ GridTest/Shared/WeatherForecast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GridTest.Shared
+{
+    public class WeatherForecast
+    {
+        public Guid key { get; set; } 
+        public string Date { get; set; }
+
+        public string TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public string TemperatureF { get; set; }
+    }
+}
